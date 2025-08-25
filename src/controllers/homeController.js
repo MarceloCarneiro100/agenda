0aeef07ba593e2ce7be4da43d48d66d334b8a744
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2025-08-25 20:37:43 -0300

    Feat: exibe o total de contatos no final da listagem

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,5 +2,6 @@ const Contato = require('../models/ContatoModel');
 
 exports.index = async (req, res) => {
     const contatos = await Contato.buscaContatosPorUsuario(req.session.user._id);
-    res.render('index', { contatos });
+    const totalContatos = contatos.length;
+    res.render('index', { contatos, totalContatos });
 };
